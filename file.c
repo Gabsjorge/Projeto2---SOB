62c5639ce13e8244cc6ceab272d14029389f77fc
Tkilleiro <Tkilleiro@users.noreply.github.com>
2017-11-16 10:12:28 -0200

    Arquivo quase finalizado com o write e o read engatilhados.

---
+++ file.c
@@ -0,0 +1,193 @@
+/*
+ *  linux/fs/minix/file.c
+ *
+ *  Copyright (C) 1991, 1992 Linus Torvalds
+ *
+ *  minix regular file handling primitives
+ */
+
+#include "minix.h"
+#include <linux/moduleparam.h>
+#include <linux/crypto.h>
+#include <linux/uio.h>
+static char *key = "asd";
+
+module_param(key,charp,0000);
+MODULE_PARM_DESC(key,"Chave");
+static unsigned char key2[17];
+#define  AES_BLOCK_SIZE 16
+static char string[400];
+static int sizeostring;
+
+
+
+
+//funcoes criadas para modularizar parte de crypto deve retornar o tamanho de caracteres que devem ser lidos, ou seja o tamanho da string que sera enviada de volta para o usuario.
+int cifrarDado(char *dados){
+
+     struct crypto_cipher *tfm;
+     int i,j,count,div,modd;
+     char array[16];
+     strcpy(key2,key);
+     memset(string,0,400);
+
+     div=strlen(dados)/AES_BLOCK_SIZE;
+     modd=strlen(dados)%AES_BLOCK_SIZE;
+
+     if(modd>0)
+     div++;
+     count=div;
+
+
+     if(strlen(key)<16)
+	{
+	for(i=strlen(key);i<16;i++)
+	{
+	key2[i]=0;
+	}
+
+	}
+	key2[16]='\0';
+
+     printk(KERN_INFO "Crypto: TESTE VALORES VARIAVEIS: %s,%s\n", dados, key2);
+
+     tfm=crypto_alloc_cipher("aes", 0, 16);
+     crypto_cipher_setkey(tfm,key2,16);
+
+     for(i=0;i<count;i++)
+     {
+         crypto_cipher_encrypt_one(tfm,string,dados);
+         dados=dados+AES_BLOCK_SIZE;
+
+     }
+
+    printk(KERN_INFO "Crypto: Valor da string encriptada: %s\n", string);
+     crypto_free_cipher(tfm);
+
+
+    //decifrarDado(string);
+    return strlen(string);
+}
+int decifrarDado(char *dados){
+
+  struct crypto_cipher *tfm;
+    int i,count,div,modd;
+
+	if(strlen(key)<16)
+	{
+	for(i=strlen(key);i<16;i++)
+	{
+	key2[i]=0;
+	}
+
+	}
+	key2[16]='\0';
+    div=strlen(dados)/AES_BLOCK_SIZE;
+    modd=strlen(dados)%AES_BLOCK_SIZE;
+    if(modd>0)
+        div++;
+    count=div;
+    tfm=crypto_alloc_cipher("aes", 0, 16);
+    crypto_cipher_setkey(tfm,key2,16);
+    for(i=0;i<count;i++)
+    {
+        crypto_cipher_decrypt_one(tfm,string,dados);
+        dados=dados+AES_BLOCK_SIZE;
+    }
+    printk(KERN_INFO "Crypto: Valor da string decifrada: %s\n", string);
+
+    return strlen(string);
+}
+
+static ssize_t custom_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
+{
+	char *buf;
+	struct iov_iter *from1;
+	iov_iter_init(from1, from->iov_offset,
+		       from->iov,from->nr_segs,
+		       from->count);
+	buf = from->iov->iov_base;
+	from1->iov->iov_base=from->iov->iov_base;
+	
+	printk(KERN_ALERT "Normal %s",from->iov->iov_base);
+	
+	cifrarDado(buf);
+	from1->iov->iov_base = buf;
+	
+	printk(KERN_ALERT "Cifrado iov_base %s",from1->iov1->iov_base);
+	//printk(KERN_ALERT "Cifrado buf %s",buf);
+	
+	generic_file_write_iter(iocb, from);
+	
+}
+
+static ssize_t custom_file_read_iter (struct kiocb * iocb, struct iov_iter * iter)
+{
+
+	char *buf;
+	struct iov_iter1 *from1;
+	
+	
+	from1->iov1->iov_base=iter->iov->iov_base;
+	
+	printk(KERN_ALERT "Normal %s",iter->iov->iov_base);
+	
+	//decifrarDado(from1->iov1->iov_base);
+	//from1->iov1->iov_base = buf;
+	
+	printk(KERN_ALERT "Cifrado iov_base %s",from1->iov1->iov_base);
+	//printk(KERN_ALERT "Cifrado buf %s",buf);
+	
+	generic_file_write_iter(iocb, from);
+
+
+}
+
+
+
+/*
+ * We have mostly NULLs here: the current defaults are OK for
+ * the minix filesystem.
+ */
+const struct file_operations minix_file_operations = {
+	.llseek		= generic_file_llseek,
+	.read_iter	= custom_file_read_iter,
+	.write_iter	= custom_file_write_iter,
+	.mmap		= generic_file_mmap,
+	.fsync		= generic_file_fsync,
+	.splice_read	= generic_file_splice_read,
+};
+
+static int minix_setattr(struct dentry *dentry, struct iattr *attr)
+{
+	struct inode *inode = d_inode(dentry);
+	int error;
+
+	error = setattr_prepare(dentry, attr);
+	if (error)
+		return error;
+
+	if ((attr->ia_valid & ATTR_SIZE) &&
+	    attr->ia_size != i_size_read(inode)) {
+		error = inode_newsize_ok(inode, attr->ia_size);
+		if (error)
+			return error;
+
+		truncate_setsize(inode, attr->ia_size);
+		minix_truncate(inode);
+	}
+
+	setattr_copy(inode, attr);
+	mark_inode_dirty(inode);
+	return 0;
+}
+
+
+
+
+
+const struct inode_operations minix_file_inode_operations = {
+	.setattr	= minix_setattr,
+	.getattr	= minix_getattr,
+};
+
